a418de0dc11c5124b835959b5b430b027292faa1
Константин Капчин <kkapchin@Konstantins-MacBook-Air.local>
2023-03-11 20:25:37 -0500

    01-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      APP component
+      <Rating />
     </div>
   );
+}
+
+function Rating() {
+  return (
+    <div>
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+      <Star />
+    </div>
+  )
+}
+
+function Star() {
+  return (
+    <div>star</div>
+  )
 }
 
 export default App;
