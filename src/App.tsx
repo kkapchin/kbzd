eba786bd597bbe3bf08d6d9ceb4bd846b3d61504
kkapchin <kkapchin@yahoo.com>
2023-03-26 01:14:03 -0400

    minor refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Accordion from './components/Accordion/Accordion';
-import Rating from "./components/Rating/Rating";
+import Accordion from './components/accordion/Accordion';
+import Rating from "./components/rating/Rating";
 
 export default function App() {
     return (
